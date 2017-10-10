8d51c4dc5ce6e71d3dd9e7587dd538080474ed97
edramos-97 <up201505779@fe.up.pt>
2017-10-10 16:58:18 +0100

    screenshots e output de tabuleiros simplificada

--- T1 - CONSTA/draw_board.pl
+++ T1 - CONSTA/draw_board.pl
@@ -4,7 +4,7 @@ getCharacter(white, 'w').
 getCharacter(white2, 'W').
 getCharacter(empty, ' ').
 
-emptyBoard([[empty,empty,empty,empty,empty,empty,empty,empty,empty],
+boards(empty,[[empty,empty,empty,empty,empty,empty,empty,empty,empty],
 			[empty,empty,empty,empty,empty,empty,empty,empty,empty],
 			[empty,empty,empty,empty,empty,empty,empty,empty,empty],
 			[empty,empty,empty,empty,empty,empty,empty,empty,empty],
@@ -14,14 +14,35 @@ emptyBoard([[empty,empty,empty,empty,empty,empty,empty,empty,empty],
 			[empty,empty,empty,empty,empty,empty,empty,empty,empty],
 			[empty,empty,empty,empty,empty,empty,empty,empty,empty]]).
 
+boards(earlygame,[[empty,empty,empty,empty,empty,empty,empty,empty,empty],
+			[empty,empty,empty,empty,empty,empty,empty,empty,empty],
+			[empty,empty,empty,empty,empty,empty,empty,empty,empty],
+			[white2,empty,empty,empty,empty,empty,empty,white,empty],
+			[empty,empty,empty,empty,black2,empty,empty,empty,empty],
+			[empty,empty,empty,empty,empty,empty,empty,empty,empty],
+			[empty,empty,empty,empty,empty,empty,black,empty,empty],
+			[empty,empty,empty,empty,empty,empty,empty,empty,empty],
+			[empty,empty,empty,empty,empty,empty,empty,empty,empty]]).
+
+boards(endgame,[[empty,empty,empty,empty,empty,empty,empty,empty,empty],
+			[empty,empty,empty,empty,empty,empty,black,empty,empty],
+			[empty,empty,empty,empty,white,white2,black,empty,empty],
+			[white2,white,white,white,white,black2,white2,white,white],
+			[empty,empty,empty,empty,black2,black,empty,empty,empty],
+			[empty,empty,empty,empty,empty,black,empty,empty,empty],
+			[empty,empty,empty,empty,empty,black,black,empty,empty],
+			[empty,empty,empty,empty,empty,empty,empty,empty,empty],
+			[empty,empty,empty,empty,empty,empty,empty,empty,empty]]).
+
+
 printColumnNames :-         write('      A     B     C     D     E     F     G     H     I   '),nl.
 printInitialSeparator :- write('    _____________________________________________________ '),nl.
 printTopLine :-          write('   |     |     |     |     |     |     |     |     |     |'),nl.
+printFinalSeparator :-   write('   |_____|_____|_____|_____|_____|_____|_____|_____|_____|'),nl.
+
 printLineRecursive([]) :- write('|'),nl.
 printLineRecursive([Cabeca|Cauda]) :- write('|  '), getCharacter(Cabeca,Simbolo), put_char(Simbolo), write('  '), printLineRecursive(Cauda).
 printLineX(Numero,Lista) :- write(' '), X is Numero+48, put_code(X), write(' '), printLineRecursive(Lista).
-printFinalSeparator :-   write('   |_____|_____|_____|_____|_____|_____|_____|_____|_____|'),nl.
 printFullLine(Numero,Lista) :- printTopLine, printLineX(Numero, Lista), printFinalSeparator.
-
 printBoardRecursive([Cabeca|Cauda],Numero) :- printFullLine(Numero,Cabeca), X is Numero+1, X<10, printBoardRecursive(Cauda,X).
-printBoard(Board) :- printColumnNames, printInitialSeparator, printBoardRecursive(Board,1).
+printBoard(Type) :- boards(Type,Board), printColumnNames, printInitialSeparator, printBoardRecursive(Board,1).
