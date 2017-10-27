2e9e47dc2c69470e5e4741a21760c6283be8eea7
Xavier Fontes <xfontes42@gmail.com>
2017-10-27 15:26:19 +0100

    update

--- T1 - CONSTA/utilities.pl
+++ T1 - CONSTA/utilities.pl
@@ -1,18 +1,12 @@
-% le_numero_enter(13,_Accumulador).
-% le_numero_enter(10,_Accumulador).
-
-
-% le_numero_enter(X) :- name(Lista_Numeros, X), write(Lista_Numeros), write(X).
-
-% le_todos_chars(10,0).
-% le_todos_chars(13,0).
-% le_todos_chars(Char, [Char| Mais_Chars]) :- get_code(Novo_Char), le_todos_chars(Novo_Char, Mais_Chars).
-% le_frase(Frase) :- get_code(Char), le_todos_chars(Char, Lista_Caracteres), name(Frase,Lista_Caracteres).
-%
-
-
 ite(If, Then, _Else) :- If, !, Then.
 ite(_If, _Then, Else) :- Else.
+
+le_todos_chars(10,0).
+le_todos_chars(13,0).
+le_todos_chars(Char, [Char| Mais_Chars]) :- get_code(Novo_Char), le_todos_chars(Novo_Char, Mais_Chars).
+le_frase(Frase) :- get_code(Char), le_todos_chars(Char, Lista_Caracteres), name(Frase,Lista_Caracteres).
+
+
 
 le_todos_numeros(Acc, Resultado):-
   get_code(Char),
