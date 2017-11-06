248c902e43e560f5f2208e9e9125eddc80b0d3c1
Xavier Fontes <xfontes42@gmail.com>
2017-11-06 09:45:06 +0000

    fixed getting the points

--- T1 - CONSTA/board.pl
+++ T1 - CONSTA/board.pl
@@ -135,6 +135,9 @@ valid_move(Matrix, X, Y, Value, New_Element):-
 % get_path_points_black(+Matrix, +Secondary_Matrix, +X_Black, +Current_Y, ?Current_Points)
 get_path_points_black(_,[],_,_,Current_Points):-
   update_max_points(Current_Points).
+  % New_Current_Points is Current_Points + 1,
+  % update_max_points(New_Current_Points).
+
 get_path_points_black(Matrix, [Row_Ahead_Of_Current_Y| Rest], X , Current_Y, Current_Points):-
   New_Y is Current_Y + 1,
   X_Diagonal_Left is X-1,
@@ -144,14 +147,15 @@ get_path_points_black(Matrix, [Row_Ahead_Of_Current_Y| Rest], X , Current_Y, Cur
     (logic_or(nth0(X_Found,Row_Ahead_Of_Current_Y,black), nth0(X_Found, Row_Ahead_Of_Current_Y, black2))),
     % THEN
     (
+      logic_or(nth0(X_Found_2,Row_Ahead_Of_Current_Y,black), nth0(X_Found_2, Row_Ahead_Of_Current_Y, black2)),
       ite(
         % IF
-        ((X_Found == X_Diagonal_Left);(X_Found == X_Diagonal_Right);(X_Found == X)),
+        ((X_Found_2 == X_Diagonal_Left);(X_Found_2 == X_Diagonal_Right);(X_Found_2 == X)),
         % THEN
         (
           % Verificar se ganha possivel crosscut
-          get_element_at(Matrix, X_Found, New_Y, Elem1),
-          get_element_at(Matrix, X_Found, Current_Y, Elem2),
+          get_element_at(Matrix, X_Found_2, New_Y, Elem1),
+          get_element_at(Matrix, X_Found_2, Current_Y, Elem2),
           get_element_at(Matrix, X, New_Y, Elem3),
           get_element_at(Matrix, X, Current_Y, Elem4),
           get_points_from_square(Elem1, Elem2, Elem3, Elem4, Points_White, Points_Black),
@@ -162,19 +166,20 @@ get_path_points_black(Matrix, [Row_Ahead_Of_Current_Y| Rest], X , Current_Y, Cur
             (
               New_Current_Points is Current_Points+1,
               update_max_points(New_Current_Points),
-              get_path_points_black(Matrix, Rest, X_Found, New_Y, New_Current_Points)
+              get_path_points_black(Matrix, Rest, X_Found_2, New_Y, New_Current_Points)
             ),
             % ELSE - black is not in control, move forward but reset max
             (
-              get_path_points_black(Matrix, Rest, X_Found, New_Y, 1)
+              get_path_points_black(Matrix, Rest, X_Found_2, New_Y, 1)
             )
           )
         ),
         % ELSE - found black pieces but not on a path
         (
-          get_path_points_black(Matrix, Rest, X_Found, New_Y, 1)
+          get_path_points_black(Matrix, Rest, X_Found_2, New_Y, 1)
         )
-      )
+      ),
+      fail
     ),
     % ELSE - did not find black pieces in this row, restart
     (New_Y_Rest is New_Y + 1, searh_black_points(Matrix, Rest, New_Y_Rest))
@@ -183,106 +188,104 @@ get_path_points_black(Matrix, [Row_Ahead_Of_Current_Y| Rest], X , Current_Y, Cur
 % searh_black_points(+Matrix, +Secondary_Matrix, Current_Line)
 searh_black_points(_, [], _).
 searh_black_points(Matrix,[Current_Row|Rest],Current_Y):-
-  ite(
-  % IF
-  (logic_or(nth0(X_Found,Current_Row,black), nth0(X_Found,Current_Row,black2))),
-  % THEN
-  ( update_max_points(1),
-    get_path_points_black(Matrix,Rest,X_Found,Current_Y,1)
-  ),
-  % ELSE
-  (Next_Y is Current_Y +1, searh_black_points(Matrix,Rest,Next_Y))
-  ).
-
-
-                        % get_points_black(+Matrix)
-                        % get_points_black(_, [], _, _, _):-
-                        %   max_points(X),
-                        %   Y is X-1,
-                        %   set_max_points(Y).
-                        %
-                        % get_points_black(Matrix, [First_Row|Rest], Current_Number, X, Y):-
-                        %   Y2 is Y + 1,
-                        %   ite((Current_Number == 0),
-                        %   ( % THEN EXTERIOR
-                        %     ite(
-                              % (logic_or(nth0(X3,First_Row,black), nth0(X3,First_Row,black2))),
-                        %       ( % THEN INTERIOR
-                        %         New_Current_Number is Current_Number + 1,
-                        %         update_max_points(New_Current_Number),
-                        %         get_points_black(Matrix, [First_Row|Rest], New_Current_Number, X3, Y)
-                        %       ), % END_THEN INTERIOR
-                        %       (
-                        %       update_max_points(Current_Number),
-                        %       get_points_black(Matrix,Rest,0,0,Y2)
-                        %       ) % ELSE INTERIOR
-                        %     )
-                        %   ), % END_THEN EXTERIOR
-                        %   ( % ELSE EXTERIOR
-                        %   ite(
-                        %     % IF
-                        %     (logic_or(nth0(X2, First_Row, black), nth0(X2, First_Row, black2))),
-                        %     % THEN
-                        %     (
-                        %         X_Diagonal_Left is X-1,
-                        %         X_Diagona_Right is X+1,
-                        %
-                        %         ite(
-                        %           % IF
-                                  % ((X2 == X_Diagonal_Left);(X2 == X_Diagona_Right);(X2 == X)),
-                        %           % THEN
-                        %           (
-                        %               get_element_at(Matrix, X2, Y, Elem1),
-                        %               get_element_at(Matrix, X2, Y2, Elem2),
-                        %               get_element_at(Matrix, X, Y, Elem3),
-                        %               get_element_at(Matrix, X, Y2, Elem4),
-                        %               get_points_from_square(Elem1, Elem2, Elem3, Elem4, Points_White, Points_Black),
-                        %               ite(
-                        %                 % IF
-                        %                 (Points_White @=< Points_Black),
-                        %                 % THEN
-                        %                 (
-                        %                   New_Current_Number is Current_Number + 1,
-                        %                   update_max_points(New_Current_Number),
-                        %                   get_points_black(Matrix, Rest, New_Current_Number, X2, Y2)
-                        %                 ),
-                        %                 % ELSE
-                        %                 (
-                        %                   update_max_points(Current_Number),
-                        %                   get_points_black(Matrix,Rest,0,0,Y2)
-                        %                 )
-                        %               )
-                        %           ),
-                        %           % ELSE
-                        %           (
-                        %               update_max_points(Current_Number),
-                        %               get_points_black(Matrix,Rest,0,0,Y2)
-                        %           )
-                        %          )
-                        %
-                        %       ),
-                        %       % ELSE
-                        %       (
-                        %         get_points_black(Matrix,Rest,0,0,Y2)) % ELSE INTERIOR
-                        %       )
-                        %   )).  % END_ELSE EXTERIOR
+    ite(
+    % IF
+    ((nth0(X_Found,Current_Row,black);nth0(X_Found,Current_Row,black2))),
+    % THEN
+    ( (nth0(X_Found_2,Current_Row,black);nth0(X_Found_2,Current_Row,black2)),
+      update_max_points(1),
+      get_path_points_black(Matrix,Rest,X_Found_2,Current_Y,1),
+      fail
+    ),
+    % ELSE
+    (
+      Next_Y is Current_Y +1, searh_black_points(Matrix,Rest,Next_Y))
+    ).
 
 % eval_board_black(+Matrix, -Number_Black)
 eval_board_black(Matrix, Number_Black):-
-  searh_black_points(Matrix, Matrix, 0),
+  \+searh_black_points(Matrix, Matrix, 0),
   max_points(Number_Black).
+
+% get_path_points_black(+Matrix, +Secondary_Matrix, +X_Black, +Current_Y, ?Current_Points)
+get_path_points_white(_,[],_,_,Current_Points):-
+  update_max_points(Current_Points).
+get_path_points_white(Matrix, [_Row_Ahead_Of_Current_Y| Rest], Current_X , Y, Current_Points):-
+  New_X is Current_X + 1,
+  Y_Diagonal_Up is Y-1,
+  Y_Diagonal_Down is Y+1,
+  ite(
+    % IF
+    (logic_or(get_element_at(Matrix, New_X, Y_Found_2, white), get_element_at(Matrix, New_X, Y_Found_2, white2))),
+    % THEN
+    (
+      logic_or(get_element_at(Matrix, New_X, Y_Found, white), get_element_at(Matrix, New_X, Y_Found, white2)),
+      ite(
+        % IF
+        ((Y_Found == Y_Diagonal_Up);(Y_Found == Y_Diagonal_Down);(Y_Found == Y)),
+        % THEN
+        (
+          % Verificar se ganha possivel crosscut
+          get_element_at(Matrix, Current_X, Y, Elem1),
+          get_element_at(Matrix, Current_X, Y_Found, Elem2),
+          get_element_at(Matrix, New_X, Y, Elem3),
+          get_element_at(Matrix, New_X, Y_Found, Elem4),
+          get_points_from_square(Elem1, Elem2, Elem3, Elem4, Points_White, Points_Black),
+          ite(
+            % IF
+            (Points_Black @=< Points_White),
+            % THEN - black can move forward
+            (
+              New_Current_Points is Current_Points+1,
+              update_max_points(New_Current_Points),
+              get_path_points_white(Matrix, Rest, New_X, Y_Found, New_Current_Points)
+            ),
+            % ELSE - black is not in control, move forward but reset max
+            (
+              get_path_points_white(Matrix, Rest, New_X, Y_Found, 1)
+            )
+          )
+        ),
+        % ELSE - found black pieces but not on a path
+        (
+          get_path_points_white(Matrix, Rest, New_X, Y_Found, 1)
+        )
+      ),
+      fail
+    ),
+    % ELSE - did not find white pieces in this column, restart
+    (New_X_Rest is New_X + 1, searh_white_points(Matrix, Rest, New_X_Rest))
+  ).
+
+% searh_white_points(+Matrix, +Secondary_Matrix, Current_Line)
+searh_white_points(_, [], _).
+searh_white_points(Matrix,[_Current_Row|Rest],Current_X):-
+  ite(
+  % IF
+  (logic_or(get_element_at(Matrix, Current_X, Y_Found, white), get_element_at(Matrix, Current_X, Y_Found, white2))),
+  % THEN
+  ( logic_or(get_element_at(Matrix, Current_X, Y_Found_2, white), get_element_at(Matrix, Current_X, Y_Found_2, white2)),
+    update_max_points(1),
+    get_path_points_white(Matrix,Rest,Current_X,Y_Found_2,1),
+    fail
+  ),
+  % ELSE
+  (Next_X is Current_X +1, searh_white_points(Matrix,Rest,Next_X))
+  ).
 
 % eval_board_white(+Matrix, -Number_White)
 eval_board_white(Matrix, Number_White):-
-  write('hello and goodbye'), nl,
+  \+searh_white_points(Matrix, Matrix, 0),
   max_points(Number_White).
 
 % eval_board(+Matrix, -Number_Black, -Number_White)
 eval_board(Matrix, Number_Black, Number_White):-
   set_max_points(0),
   eval_board_black(Matrix, Number_Black),
+  % write('passei black'), nl,
   set_max_points(0),
   eval_board_white(Matrix, Number_White),
+  % write('passei white'), nl,
   set_max_points(0).
   % limit(Number_Black_2, 0, 1000, Number_Black),
   % limit(Number_White_2, 0, 1000, Number_White),
@@ -330,3 +333,9 @@ check_win_white([First_Row|Rest]):-
   X is 0,
   logic_or(get_element_at([First_Row|Rest], X, Y, white), get_element_at([First_Row|Rest], X, Y, white2)),
   search_white([First_Row|Rest], Rest, X, Y).
+
+% eval_board([[ white, empty, white, empty, black],
+%             [ empty, white, black2, empty, empty],
+%             [ empty, black, empty, empty, empty],
+%             [ empty, empty, empty, empty, empty],
+%             [ empty, empty, empty, empty, black]], X2, Y2).
